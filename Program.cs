3b3855d149ff45f7c0acff82dc8650e5af1422dd
Noah Cou <noahjcou@gmail.com>
2020-02-20 20:34:01 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,142 @@
+﻿using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Exceptions;
+using DSharpPlus.Entities;
+using DSharpPlus.EventArgs;
+using DSharpPlus.VoiceNext;
+using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace ElevatorBot
+{
+    class Program
+    {
+        public DiscordClient bot;
+        public CommandsNextExtension Commands { get; set; }
+        public VoiceNextExtension Voice { get; set; }
+
+        public static Program instance = new Program();
+        static void Main(string[] args)
+        {
+            instance.startBotAsync().GetAwaiter().GetResult();
+        }
+
+        private Task botReady(ReadyEventArgs e)
+        {
+            e.Client.DebugLogger.LogMessage(LogLevel.Info, "ElevatorBot", "Bot is ready", DateTime.Now);
+
+            return Task.CompletedTask;
+        }
+
+        private Task botGuildAvailable(GuildCreateEventArgs e)
+        {
+            e.Client.DebugLogger.LogMessage(LogLevel.Info, "ElevatorBot", $"Guild Available: {e.Guild.Name}", DateTime.Now);
+
+            return Task.CompletedTask;
+        }
+
+        private Task botErrorHandler(ClientErrorEventArgs e)
+        {
+            e.Client.DebugLogger.LogMessage(LogLevel.Error, "ElevatorBot", $"Exception occurred: {e.Exception.GetType()}: {e.Exception.Message}", DateTime.Now);
+
+            return Task.CompletedTask;
+        }
+
+        private Task commandsExecuted(CommandExecutionEventArgs e)
+        {
+            e.Context.Client.DebugLogger.LogMessage(LogLevel.Info, "ElevatorBot", $"{e.Context.User.Username} issued the command '{e.Command.QualifiedName}'", DateTime.Now);
+
+            return Task.CompletedTask;
+        }
+
+        private async Task commandsError(CommandErrorEventArgs e)
+        {
+            e.Context.Client.DebugLogger.LogMessage(LogLevel.Info, "ElevatorBot", $"{e.Context.User.Username} tried executing '{e.Command?.QualifiedName ?? "<unknown command>"}' but it errored: {e.Exception.GetType()}: {e.Exception.Message ?? "<no message>"}", DateTime.Now);
+
+            if (e.Exception is ChecksFailedException ex)
+            {
+                DiscordEmoji emoji = DiscordEmoji.FromName(e.Context.Client, ":x:");
+
+                var embed = new DiscordEmbedBuilder
+                {
+                    Title = "",
+                    Description = $"{emoji} You do not have permission to use this command.",
+                    Color = new DiscordColor(0xff0000)
+                };
+
+                await e.Context.RespondAsync("", embed: embed);
+            }
+        }
+
+        public async Task startBotAsync()
+        {
+            //load json
+            JConf cjason;
+            JToken tjason;
+            using (StreamReader r = new StreamReader("config.json"))
+            {
+                var json = r.ReadToEnd();
+                cjason = JsonConvert.DeserializeObject<JConf>(json);
+            }
+            using (StreamReader r = new StreamReader("token.json"))
+            {
+                var json = r.ReadToEnd();
+                tjason = JsonConvert.DeserializeObject<JToken>(json);
+            }
+
+            var config = new DiscordConfiguration
+            {
+                Token = tjason.token,
+                TokenType = TokenType.Bot,
+
+                AutoReconnect = true,
+                LogLevel = LogLevel.Info,
+                UseInternalLogHandler = true
+            };
+
+            this.bot = new DiscordClient(config);
+
+            this.bot.Ready += this.botReady;
+            this.bot.GuildAvailable += this.botGuildAvailable;
+            this.bot.ClientErrored += this.botErrorHandler;
+
+            var commandConfig = new CommandsNextConfiguration
+            {
+                StringPrefixes = new[] { cjason.prefix },
+
+                EnableMentionPrefix = true
+            };
+
+            this.Commands = this.bot.UseCommandsNext(commandConfig);
+
+            this.Commands.CommandExecuted += this.commandsExecuted;
+            this.Commands.CommandErrored += this.commandsError;
+
+            this.Commands.RegisterCommands<ElevatorBotCommands>();
+            this.Commands.RegisterCommands<ElevatorBotVoice>();
+            
+            var voiceConfig = new VoiceNextConfiguration
+            {
+                AudioFormat = AudioFormat.Default
+            };
+
+            this.Voice = this.bot.UseVoiceNext(voiceConfig);
+
+            await this.bot.ConnectAsync();
+
+            //prevent bot from stopping
+            await Task.Delay(-1);
+        }
+    }
+    class JConf
+    {
+        // class and variables for json configs
+        public string prefix = "";
+    }
+    class JToken
+    {
+        public string token = "";
+    }
+}
---
+++ commands.cs
@@ -0,0 +1,146 @@
+﻿using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using DSharpPlus.Entities;
+using DSharpPlus.VoiceNext;
+using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ElevatorBot
+{
+    public class ElevatorBotCommands : BaseCommandModule
+    {
+        [Command("ping")] // let's define this method as a command
+        [Description("Example ping command")] // this will be displayed to tell users what this command does when they invoke help
+        [Aliases("pong")] // alternative names for the command
+        public async Task Ping(CommandContext ctx) // this command takes no arguments
+        {
+            //trigger a typing indicator to let users know 
+            await ctx.TriggerTypingAsync();
+
+            var emoji = DiscordEmoji.FromName(ctx.Client, ":ping_pong:");
+
+            // respond with ping
+            await ctx.RespondAsync($"{emoji} Pong! Ping: {ctx.Client.Ping}ms");
+        }
+    }
+
+    public class ElevatorBotVoice : BaseCommandModule
+    {
+        [Command("connect")]
+        [Description("Play elevator music")]
+        [Aliases("join", "start", "play")]
+        public async Task Start(CommandContext ctx, string Choice, DiscordChannel channel = null)
+        {
+            string[] musicType = { "kevin", "ben", "portal" };
+
+            var vnext = ctx.Client.GetVoiceNext();
+            var vnc = vnext.GetConnection(ctx.Guild);
+            if (vnext == null)
+            {
+                await ctx.RespondAsync("Voice is not enabled for this bot.");
+                return;
+            }
+
+            if (vnc != null)
+            {
+                await ctx.RespondAsync("Already connected to a channel.");
+                return;
+            }
+
+            var vstat = ctx.Member?.VoiceState;
+            if (vstat?.Channel == null && channel == null)
+            {
+                await ctx.RespondAsync("You are not in a voice channel.");
+                return;
+            }
+
+            if (channel == null)
+                channel = vstat.Channel;
+
+            Choice = Choice.ToLower();
+            if (!musicType.Any(x => Choice.Contains(x))) {
+                await ctx.RespondAsync($"Please pick a music type, choices are: `Kevin` (MacLeod), `Ben`(Sounds), and `Portal` Radio");
+                return;
+            }
+
+            vnc = await vnext.ConnectAsync(channel);
+            // await ctx.RespondAsync($"Connected to `{channel.Name}");
+            int rand = new Random().Next(1, 2);
+            if (rand == 1)
+            {
+                await ctx.RespondAsync($"`{channel.Name}` Going up!");
+            } else
+            {
+                await ctx.RespondAsync($"`{channel.Name}` Going down!");
+            } 
+
+            // play
+            // await ctx.Message.RespondAsync($"Playing Elevator Music");
+            await vnc.SendSpeakingAsync(true);
+
+            //loop elevator
+            do
+            {
+                var psi = new ProcessStartInfo
+                {
+                    FileName = "ffmpeg",
+                    Arguments = $"-i \"elevator-{Choice}.mp3\" -ac 2 -f s16le -ar 48000 pipe:1",
+                    RedirectStandardOutput = true,
+                    UseShellExecute = false
+                };
+
+                var ffmpeg = Process.Start(psi);
+                var ffout = ffmpeg.StandardOutput.BaseStream;
+                VoiceTransmitStream vStream = vnc.GetTransmitStream(20);
+
+                var buff = new byte[3840];
+                var br = 0;
+                while ((br = ffout.Read(buff, 0, buff.Length)) > 0)
+                {
+                    if (br < buff.Length) // not a full sample, mute the rest
+                        for (var i = br; i < buff.Length; i++)
+                            buff[i] = 0;
+
+                    await vStream.WriteAsync(buff);
+                }
+
+                await vnc.SendSpeakingAsync(false); // we're not speaking anymore
+
+                while (vnc.IsPlaying)
+                {
+                    await vnc.WaitForPlaybackFinishAsync();
+                }
+            } while (true);
+        }
+
+        [Command("stop")]
+        [Description("Stop playing music")]
+        [Aliases("disconnect", "quit", "end")]
+        public async Task End(CommandContext ctx)
+        {
+            var voiceNext = ctx.Client.GetVoiceNext();
+            if (voiceNext == null)
+            {
+                // not enabled
+                await ctx.RespondAsync("VNext is not enabled or configured.");
+                return;
+            }
+
+            // check whether we are connected
+            var voiceConnection = voiceNext.GetConnection(ctx.Guild);
+            if (voiceConnection == null)
+            {
+                // not connected
+                await ctx.RespondAsync("Not connected in this server.");
+                return;
+            }
+
+            string channel = voiceConnection.Channel.Name;
+            // disconnect
+            voiceConnection.Disconnect();
+            await ctx.RespondAsync($"`{channel}` has reached their floor");
+        }
+    }
+}
